52c2ec8467570fe8a68b3499d52d89b63f724589
Alvaro VM <alvarovm@gmail.com>
2014-06-26 15:43:47 +1000

    And Batch for installing gulp dependencies in PC

---
+++ helper/installGulpDependencies.bat
@@ -0,0 +1,10 @@
+call npm install --save-dev gulp
+call npm install --save-dev gulp-util
+call npm install --save-dev gulp-concat
+call npm install --save-dev gulp-browserify
+call npm install --save-dev gulp-compass
+call npm install --save-dev gulp-connect
+call npm install --save-dev gulp-if
+call npm install --save-dev gulp-uglify
+call npm install --save-dev gulp-minify-html
+call npm install --save-dev jquery
